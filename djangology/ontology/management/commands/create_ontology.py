d7ad5a89f2413169a8d60d50fbe080f735fb22b3
Open Risk <openrisk@outlook.com>
2025-12-08 12:51:09 +0100

    dynamic creation of account ontology (classes only)

--- djangology/ontology/management/commands/create_ontology.py
+++ djangology/ontology/management/commands/create_ontology.py
@@ -3,22 +3,28 @@ from owlready2 import *
 
 
 class Command(BaseCommand):
-    help = 'create a deb ontology'
+    help = 'create a DEB accounting ontology'
 
     def handle(self, *args, **kwargs):
+
+        # Create a new root ontology of accounts
+        onto = get_ontology("http://www.openriskmanual.org/ns/account_ontology.owl")
+        onto_classes = get_ontology("http://www.openriskmanual.org/ns/accounts.owl")
+        onto_classes.imported_ontologies.append(onto)
+
         stocks = ['Asset', 'Liability', 'Equity', 'Inventory', 'Cash', 'Cash Equivalent', 'Trade Receivable',
                   'Share Capital', 'Retained Earnings', 'Other Equity', 'Borrowing', 'Provision', 'Lease', 'Pension',
                   'Deferred Tax', 'Income Tax', 'Property', 'Plant', 'Equipment', 'Goodwill']
         flows = ['Revenue', 'Other Income', 'Expense', 'Loss', 'Cost of Sales', 'Depreciation']
         modifiers = ['Current', 'Non-current', 'Intangible', 'Investment', 'Financing', 'Operating']
 
-        # Create a new ontology
-        onto = get_ontology("http://www.openriskmanual.org/ns/account_ontology.owl")
-
-        # Define a class
         with onto:
-            class Account(Thing):
-                pass
+            for entity in stocks:
+                Class = types.new_class(entity, (Thing,))
+            for entity in flows:
+                Class = types.new_class(entity, (Thing,))
+            for entity in modifiers:
+                Class = types.new_class(entity, (Thing,))
 
         # Save the ontology to a file
         onto.save(file="account_ontology.owl", format="rdfxml")
